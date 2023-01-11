4d74f060db2a3d7ce7cf7313eaf2ab396ee1490d
C-Sweet-Person <106597560+C-Sweet-Person@users.noreply.github.com>
2023-01-11 10:21:34 -0400

    Updated HTML
    Made sure that login.js, login.php and .htaccess beginning to work.

--- web/login.js
+++ web/login.js
@@ -11,8 +11,31 @@ function login()
     let username = document.querySelector("#username");
     let password = document.querySelector("#password");
     console.log(username.value,password.value);
+    //Place into object
+    let account = {"username":username.value,"password":password.value};
+    console.log(account);
+    //Actual login
+    let url = "API/UserService.php"; // REST-style: URL refers to an entity or collection, not an action
+    let xmlhttp = new XMLHttpRequest();
+    let obj = {
+        "username": username,
+        "password": password
+    };
+    let method = "POST";
+     xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            let resp = xmlhttp.responseText;
+                console.log(resp);
+                if (resp === "1")
+                {
+                   alert("Dashboard page not done yet.");
+                }
+        }
+    }
+    xmlhttp.open(method, url, true); // method is either POST or PUT
+    xmlhttp.send(JSON.stringify(obj));
+  }
 
-}
 
 function showHide()
 {
@@ -20,3 +43,5 @@ function showHide()
     let bool = password.type === "password" ? "text":"password";
     password.type = bool;
 }
+
+//
