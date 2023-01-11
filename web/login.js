138c2ce8e1e2f430142d633d1cbeb32277ea5ddf
C-Sweet-Person <106597560+C-Sweet-Person@users.noreply.github.com>
2023-01-11 01:44:36 -0400

    Added first bit of HTML/PHP Files

---
+++ web/login.js
@@ -0,0 +1,8 @@
+window.onload = (event) => {
+    document.querySelector("#loginBtn").addEventListener("click",passwordCheck);
+  };
+
+function passwordCheck()
+{
+    console.log("Test");
+}
---
+++ web/login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+<img src="img/Logo.jpg" width="100px" height="100px"><br>
+<span>Username: </span><input type="text"><br>
+<span>Password: </span><input type="text"><br>
+<button id="loginBtn">Login</button>
+<script src="login.js"></script>
+</body>
+</html>
