4d8e15ee0eeff9c19890c361c3d1221d5d0a480e
C-Sweet-Person <106597560+C-Sweet-Person@users.noreply.github.com>
2023-01-11 10:22:21 -0400

    Add files via upload

---
+++ web/Entity/user.php
@@ -0,0 +1,64 @@
+<?php
+
+class User implements JsonSerializable {
+    private $employeeID;
+    private $username;
+    private $password;
+    private $firstname;
+    private $lastName;
+    private $email;
+    private $active;
+    private $locked;
+    private $positionID;
+    private $siteID;
+    private $permissionLevel;
+
+    public function __construct( $employeeID, $username, $password, $firstname,$lastname,$email,$active,$locked,$positionID,$siteID) {
+        $this->employeeID = $employeeID;
+        $this->username = $username;
+        $this->password = $password;
+        $this->firstname = $firstname;
+        $this->lastName = $lastname;
+        $this->email = $email;
+        $this->active = $active;
+        $this->locked = $locked;
+        $this->positionID = $positionID;
+        $this->siteID = $siteID;
+    }
+    public function getID()
+    {
+        return $this->employeeID;
+    }
+    public function getUsername() {
+        return $this->username;
+    }
+
+    public function getPassword() {
+        return $this->password;
+    }
+    public function getFirstName() {
+        return $this->firstname;
+    }
+    public function getLastName() {
+        return $this->lastName;
+    }
+    public function getEmail() {
+        return $this->email;
+    }
+    public function getActive() {
+        return $this->active;
+    }
+    public function getLocked() {
+        return $this->locked;
+    }
+    public function getPositionID() {
+        return $this->positionID;
+    }
+    public function getSiteID() {
+        return $this->siteID;
+    }
+
+    public function jsonSerialize() {
+        return get_object_vars($this);
+    }
+}
