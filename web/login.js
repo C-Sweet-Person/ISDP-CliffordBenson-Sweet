45b5f625b97f1a6ea2cca4ffc8b02004e9c48184
C-Sweet-Person <106597560+C-Sweet-Person@users.noreply.github.com>
2023-01-11 09:33:07 -0400

    Add files via upload
    Added the login page with password toggability.

--- web/login.js
+++ web/login.js
@@ -1,8 +1,22 @@
 window.onload = (event) => {
-    document.querySelector("#loginBtn").addEventListener("click",passwordCheck);
+    document.querySelector("#loginBtn").addEventListener("click",login);
+    document.querySelector("#passwordVisibility").addEventListener("click",showHide);
   };
 
-function passwordCheck()
+
+
+function login()
 {
-    console.log("Test");
+    //Set variables.
+    let username = document.querySelector("#username");
+    let password = document.querySelector("#password");
+    console.log(username.value,password.value);
+
+}
+
+function showHide()
+{
+    let password = document.querySelector("#password");
+    let bool = password.type === "password" ? "text":"password";
+    password.type = bool;
 }
