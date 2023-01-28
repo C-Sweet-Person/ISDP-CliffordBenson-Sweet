37ea8074a0e30fe78ad8041bc46222a008b991cc
C-Sweet-Person <106597560+C-Sweet-Person@users.noreply.github.com>
2023-01-28 14:20:17 -0400

    Created Transaction constructor.

--- ISDP_FinalProject/Transaction.cs
+++ ISDP_FinalProject/Transaction.cs
@@ -12,10 +12,23 @@ namespace ISDP_FinalProject
         private int txnID; 
         private string txnType;
         private string status; 
-        private int txnDate;
+        private string txnDate;
         private int SiteID;
         private int deliveryID;
         private int employeeID;
         private string notes;
+        public Transaction(int txnAuditID, int txnID, string txnType, string status, string txnDate, int SiteID, int deliveryID, int employeeID, string notes)
+        {
+            this.txnAuditID = txnAuditID;
+            this.txnID = txnID;
+            this.txnType = txnType;
+            this.status = status;
+            this.txnDate = txnDate;
+            this.SiteID = SiteID;
+            this.deliveryID = deliveryID;
+            this.employeeID = employeeID;
+            this.notes = notes;
+
+        }
     }
 }
