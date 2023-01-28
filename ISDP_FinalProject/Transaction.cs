5f4dd137edd66426ada467b9ea52e616d89b6d4e
C-Sweet-Person <106597560+C-Sweet-Person@users.noreply.github.com>
2023-01-28 14:10:48 -0400

    Added Transaction class

---
+++ ISDP_FinalProject/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISDP_FinalProject
+{
+    internal class Transaction
+    {
+        private int txnAuditID; 
+        private int txnID; 
+        private string txnType;
+        private string status; 
+        private int txnDate;
+        private int SiteID;
+        private int deliveryID;
+        private int employeeID;
+        private string notes;
+    }
+}
