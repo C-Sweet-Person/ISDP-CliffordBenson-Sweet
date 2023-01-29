2548b6a5a0c4ec62c12259a5418ba0ecf8a429d9
C-Sweet-Person <106597560+C-Sweet-Person@users.noreply.github.com>
2023-01-29 13:18:15 -0400

    Added admin password class.

---
+++ ISDP_FinalProject/Encrpytion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISDP_FinalProject
+{
+    internal class Encrpytion
+    {
+        public static string Encrypt()
+        {
+
+        }
+        public static string Dencrypt()
+        {
+
+        }
+    }
+}
