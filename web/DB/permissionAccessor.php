96d3e5747b11b57bdfeac1f47296d48fe43f6c3c
C-Sweet-Person <106597560+C-Sweet-Person@users.noreply.github.com>
2023-01-31 11:56:27 -0400

    Sprint 2 prep

---
+++ web/DB/permissionAccessor.php
@@ -0,0 +1,22 @@
+<?php
+require_once 'DatabaseConnecter.php';
+require_once '../Entity/User.php';
+
+class permissionAccessor
+{
+    private $conn = NULL;
+    public function __construct()
+    {
+        $cm = new Connector();
+
+        $this->conn = $cm->connect_db();
+        if (is_null($this->conn)) {
+            throw new Exception("no connection");
+        }
+    }
+    public function getPermissionsOfUser()
+    {
+        
+    }
+}
+?>
