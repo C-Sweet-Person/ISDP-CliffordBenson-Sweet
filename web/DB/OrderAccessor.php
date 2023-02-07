c3eae12e299abafb43739dcd38ca508d75267eac
C-Sweet-Person <106597560+C-Sweet-Person@users.noreply.github.com>
2023-02-07 10:50:10 -0400

    OrderAccessor filling with code

---
+++ web/DB/OrderAccessor.php
@@ -0,0 +1,30 @@
+<?php
+require_once 'DatabaseConnecter.php';
+require_once '../Entity/User.php';
+
+class OrderAccessor
+{
+    private $conn = NULL;
+    public function __construct()
+    {
+        $cm = new Connector();
+
+        $this->conn = $cm->connect_db();
+        if (is_null($this->conn)) {
+            throw new Exception("no connection");
+        }
+    }
+    public function createOrder()
+    {
+        
+    }
+    public function deleteOrder()
+    {
+        
+    }
+    public function getOrderDetails()
+    {
+        
+    }
+}
+?>
