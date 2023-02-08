e1b2937af5c7effbdaec56a6174420c7b27abaa6
C-Sweet-Person <106597560+C-Sweet-Person@users.noreply.github.com>
2023-02-08 10:02:49 -0400

    Added ordering page

---
+++ web/OrderPage.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<link rel="icon" type="image/x-icon" href="favicon.ico">
+<link rel="stylesheet" href="style.css"></link>
+<title>Order Page</title>
+</head>
+<body>
+    <table id="orderPage"></table>
+    <div id="orderDetails"></div>
+    <button id="AddItem" class="center">AddItem</button>
+    <button id="RemoveItem"class="center">RemoveItem</button>
+    <br><a href="dashboard.php"><button class="center">Go back.</button></a>
+    <script src="ordering.js"></script>
+</body>
+</html>
