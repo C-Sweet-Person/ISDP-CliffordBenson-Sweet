4e7dd1810e3f61c72300896afbc8794937272a6e
C-Sweet-Person <106597560+C-Sweet-Person@users.noreply.github.com>
2023-02-23 15:32:56 -0400

    Check commit

---
+++ ISDP_FinalProject/Admin Functions/LocationTools.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace ISDP_FinalProject.Admin_Functions
+{
+    partial class LocationTools
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnViewLocation = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnEditLocation = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnViewLocation
+            // 
+            this.btnViewLocation.Location = new System.Drawing.Point(12, 338);
+            this.btnViewLocation.Name = "btnViewLocation";
+            this.btnViewLocation.Size = new System.Drawing.Size(175, 29);
+            this.btnViewLocation.TabIndex = 0;
+            this.btnViewLocation.Text = "View Location";
+            this.btnViewLocation.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 320);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // btnEditLocation
+            // 
+            this.btnEditLocation.Location = new System.Drawing.Point(12, 373);
+            this.btnEditLocation.Name = "btnEditLocation";
+            this.btnEditLocation.Size = new System.Drawing.Size(175, 29);
+            this.btnEditLocation.TabIndex = 2;
+            this.btnEditLocation.Text = "EditLocation";
+            this.btnEditLocation.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(613, 373);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(175, 29);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Exit";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // LocationTools
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnEditLocation);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnViewLocation);
+            this.Name = "LocationTools";
+            this.Text = "LocationTools - Bullseye Sporting Goods";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button btnViewLocation;
+        private DataGridView dataGridView1;
+        private Button btnEditLocation;
+        private Button btnClose;
+    }
+}
