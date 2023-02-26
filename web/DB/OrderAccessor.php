37583834c9479f452b8126fbfbc915bc8275ebed
C-Sweet-Person <106597560+C-Sweet-Person@users.noreply.github.com>
2023-02-26 16:21:40 -0400

    Added get stuff

--- web/DB/OrderAccessor.php
+++ web/DB/OrderAccessor.php
@@ -14,9 +14,39 @@ class OrderAccessor
             throw new Exception("no connection");
         }
     }
-    public function createOrder()
+    public function getAllOrders($selectString)
     {
-        
+        $result = [];
+
+        try {
+            $stmt = $this->conn->prepare($selectString);
+            $stmt->execute();
+            $dbresults = $stmt->fetchAll(PDO::FETCH_ASSOC);
+//( $itemID, $name, $description,$category,$weight,$costPrice,$retailPrice,$supplierID,$active,$notes,$caseSize) 
+            foreach ($dbresults as $r) {
+                $itemID = $r['itemID'];
+                $name = $r['name'];
+                $description = $r['description'];
+                $category = $r['category'];
+                $weight = $r['weight'];
+                $costPrice = $r['costPrice'];
+                $retailPrice = $r['retailPrice'];
+                $supplierID = $r['supplierID'];
+                $active = $r['active'];
+                $notes = $r['notes'];
+                $caseSize = $r['caseSize'];
+                $item = new item( $itemID, $name, $description,$category,$weight,$costPrice,$retailPrice,$supplierID,$active,$notes,$caseSize);
+                array_push($result, $item);
+            }
+        } catch (Exception $e) {
+            $result = [];
+        } finally {
+            if (!is_null($stmt)) {
+                $stmt->closeCursor();
+            }
+        }
+
+        return $result;
     }
     public function deleteOrder()
     {
