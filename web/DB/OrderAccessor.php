6401c692d10bae8d5e9789411aebce8c3f10180b
C-Sweet-Person <106597560+C-Sweet-Person@users.noreply.github.com>
2023-02-28 15:27:52 -0400

    Minor sprint 3 progress.

--- web/DB/OrderAccessor.php
+++ web/DB/OrderAccessor.php
@@ -14,6 +14,11 @@ class OrderAccessor
             throw new Exception("no connection");
         }
     }
+    /*
+    This function is used
+    to get all the orders with a custom
+    select string.
+    */
     public function getAllOrders($selectString)
     {
         $result = [];
@@ -47,12 +52,43 @@ class OrderAccessor
 
         return $result;
     }
+    //
+    //
+    //
+    //
+
     public function changeStatus($orderID, $msg)
     {
         $stmt = $this->conn->prepare("update txn set status = '" . $msg . "' where txnID = " . $orderID);
         $stmt->execute();    
     }
+    /**
+     * This function is used
+     * to get the order types, but only for ones that can be specfically created.
+     */
     public function getOrderDetails()
+    {
+        
+    }
+    /**
+     * This function creates an
+     * order using a specified order type and
+     * takes in a list to create txnLine items after the
+     * actual order is created.
+     */
+    public function createOrder($items,$type)
+    {
+        $stmt = $this->conn->prepare($selectString);
+        $stmt->execute();
+        $dbresults = $stmt->fetchAll(PDO::FETCH_ASSOC);  
+    }
+    /**
+     * This function will just take in an order
+     * And a type in order to change
+     * that order's status to a particular 
+     * type
+     */
+    public function changeType($order,$type)
     {
         
     }
