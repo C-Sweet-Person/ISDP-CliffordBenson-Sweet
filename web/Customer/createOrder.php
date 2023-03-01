59a1aab39aeb7e4bde1efa0139722aef11c0140b
C-Sweet-Person <106597560+C-Sweet-Person@users.noreply.github.com>
2023-03-01 15:52:58 -0400

    Buying info

---
+++ web/Customer/createOrder.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+        <title>Shopping Area - Bullseye Sporting Goods</title>
+        <meta charset="UTF-8">
+</head>
+<body>
+    <h1>Shopping area</h1>
+    <select name = "Category"></select>
+    <table id="itemList"></select>
+    <button id="prev">Previous</button><button id="next">Next</button><br>
+    <button id="addToCart">Add to cart</button><br>
+    <button id="removeItem">Remove item</button><br>
+    <button id="orderSummery">buyNow</button><br>
+    <a href="homepage.php"><button>Back to homepage</button></a>
+</body>
+</html>
