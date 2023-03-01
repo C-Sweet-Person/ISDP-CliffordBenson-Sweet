1f580d35b35923f00cd2511444bb577335aa5c7c
C-Sweet-Person <106597560+C-Sweet-Person@users.noreply.github.com>
2023-03-01 15:52:53 -0400

    Create order buying information

---
+++ web/Customer/homepage.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Homepage - Bullseye Sporting Goods</title>
+</head>
+<body>
+<h1>Home</h1>
+<p>Welcome to Bullseye sporting goods, your place for all things
+    sporting goods. In this section, you'll be able to see which orders you've made
+    or even create a brand new order.
+</p>
+<table
+</body>
+</html>
