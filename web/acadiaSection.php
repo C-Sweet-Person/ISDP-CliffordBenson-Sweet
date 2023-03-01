3e6e83d17520bec18621b60c1a14d0a4cce6abaf
C-Sweet-Person <106597560+C-Sweet-Person@users.noreply.github.com>
2023-03-01 15:26:48 -0400

    Beginning of Sprint 3 items

---
+++ web/acadiaSection.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+    <title>Acadia - Bullseye Sporting Goods</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+<h1>Acadia Portal</h1>
+<table id="activeOrders"></table>
+<button id="Prev">Previous</button><button id="Next">Next</button>
+<ul>
+    <a id="pickupOrder">Pickup Order</a>
+    <a id="transportOrder">Transport Store Order</a>
+    <a id="deliverOrder">Deliver Store order</a>
+    <a id="viewOrder">View Order Details</a>
+</ul>
+<script src="acadia.js"></script>
+</body>    
+</html>
