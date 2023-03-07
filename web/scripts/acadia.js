77a52aed0989572d92ab021f590257f3e70409f4
C-Sweet-Person <106597560+C-Sweet-Person@users.noreply.github.com>
2023-03-06 22:05:38 -0400

    Making initial commenting and empty functions for Acadia and web ordering

---
+++ web/scripts/acadia.js
@@ -0,0 +1,35 @@
+
+window.onload = function() {
+//Call getOrders()
+}
+/**This function gets the orders that
+ * are ready to be picked up and gives
+ * a JSON that is used to build the
+ * order table
+ */
+
+function getLocations()
+{
+//Get storeLocations to deliver orders
+}
+function getOrders()
+{
+
+}
+
+/**
+ * This function creates an
+ * order table by which an order
+ * can be selected, which is what
+ * a lot of the functions check for
+ * (id .selected)
+ */
+function createOrderTable()
+{
+
+
+}
+/**
+ * This function creates the code
+ * that
+ */
---
+++ web/scripts/webOrdering.js
@@ -0,0 +1,62 @@
+
+/**
+ * This function gets the store
+ * locations that can be used with
+ * the next function, fill in stores.
+ */
+function getStores()
+{
+
+
+}
+/**
+ * This function grabs the info
+ * from getStores and places it
+ * in the dropdown box.
+ */
+function fillInStores()
+ {
+
+ }
+    /**
+     * This function will get
+     * the items into a json to be
+     * placed into a table.
+     * (This function is called
+     * whenever the dropdown boxes
+     * changes)
+     */
+function getItemsExisting()
+{
+
+}
+/**
+ * This function displays
+ * the items into a table
+ * to select items via quantity.
+ */
+function makeItemDisplay()
+{
+
+
+}
+
+/**
+ * This function checks to see if the add button was pressed
+ * and adds an item based on if the input checkboxes values
+ * have been changed.
+ */
+function AddItem_Order()
+{
+
+
+}
+
+/**
+ * This function creates the order and sends it to the warehouse
+ * as an online order txnType as well. (Take in a list, return an alert.)
+ */
+function sendOrderInfo(items)
+{
+
+}
